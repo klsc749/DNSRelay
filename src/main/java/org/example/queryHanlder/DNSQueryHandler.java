ff8177f26fa6af27f4fee3dd8f1ac29960b44573
给贝拉嘉晚饭 <1554208383@qq.com>
2023-06-26 16:29:29 +0800

    Complete RemoteServerHandler

--- src/main/java/org/example/queryHanlder/DNSQueryHandler.java
+++ src/main/java/org/example/queryHanlder/DNSQueryHandler.java
@@ -1,8 +1,9 @@
 package org.example.queryHanlder;
 
-import org.example.queryHanlder.model.DNSRecord;
+import org.example.dns.model.Message;
+import org.example.dns.model.Question;
 
 public interface DNSQueryHandler {
-    DNSRecord handle(String domainName);
+    Message handle(Question question);
     void setNext(DNSQueryHandler next);
 }
--- src/main/java/org/example/queryHanlder/remoteServerHandler/RemoteServerHandler.java
+++ src/main/java/org/example/queryHanlder/remoteServerHandler/RemoteServerHandler.java
@@ -1,10 +1,20 @@
 package org.example.queryHanlder.remoteServerHandler;
 
+import org.example.datagramSocketPool.DatagramSocketPool;
+import org.example.dns.model.*;
+import org.example.dns.sender.DNSQuerySender;
+import org.example.dns.sender.DNSQuerySenderI;
 import org.example.queryHanlder.DNSQueryHandler;
-import org.example.queryHanlder.model.DNSRecord;
+
+import java.net.DatagramSocket;
+import java.util.ArrayList;
 
 public class RemoteServerHandler implements DNSQueryHandler {
     private DNSQueryHandler next;
+    private DatagramSocketPool datagramSocketPool;
+    public RemoteServerHandler(DatagramSocketPool datagramSocketPool) {
+        this.datagramSocketPool = datagramSocketPool;
+    }
 
     @Override
     public void setNext(DNSQueryHandler next) {
@@ -12,10 +22,34 @@ public class RemoteServerHandler implements DNSQueryHandler {
     }
 
     @Override
-    public DNSRecord handle(String domainName) {
-        // 实现查询远程DNS服务器的逻辑...
-        // 如果查询到结果，返回
-        // 否则，调用下一个处理器的handle方法
-        return null;
+    public Message handle(Question question) {
+        Message message = new Message();
+        Header header = new Header();
+        header.setFlags(new Flag());
+        header.getFlags().setQr(false);
+        header.setQdCount(1);
+        message.setHeader(header);
+        message.setSections(new ArrayList<>());
+        message.getSections().add(question);
+
+        DNSQuerySender sender = new DNSQuerySenderI();
+
+        Message response;
+        DatagramSocket socket = null;
+        try{
+            socket = datagramSocketPool.getDatagramSocket();
+            response = sender.sendQuery(message, socket);
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }finally{
+            if(socket != null){
+                datagramSocketPool.returnDatagramSocket(socket);
+            }
+        }
+
+        //TODO: update redis
+
+        return response;
     }
 }
